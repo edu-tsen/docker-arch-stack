9f77de87a77298501bacc6dc16d8546694d71b8a
Eduardo Tsen <edu@edu-tsen.de>
2016-02-28 01:00:34 +0100

    Change mkimage-arch.sh
    - use pacman's NoExtract option instead of localepurge
    - generate pacman.conf on-the-fly

--- archlinux/README.md
+++ archlinux/README.md
@@ -2,14 +2,11 @@
 
 Generates a minimal arch base image.
 
-Based on [mkimage-arch.sh](https://github.com/docker/docker/blob/master/contrib/mkimage-arch.sh)
-with [Arch Linux Archive](https://wiki.archlinux.org/index.php/Arch_Linux_Archive) as snapshot repository
-and cleaned up with [localepurge](https://packages.debian.org/source/sid/localepurge).
+Following adaptions were made on [mkimage-arch.sh](https://github.com/docker/docker/blob/master/contrib/mkimage-arch.sh):
+* Use [Arch Linux Archive](https://wiki.archlinux.org/index.php/Arch_Linux_Archive) as snapshot repository
+* ```NoExtract``` systemd units and non-english messages
+* Clean pacman cache (```pacman -Scc```)
 
-Current version is 2016-02-26.
+Current snapshot version is 2016-02-26.
 
 [![Archlinux](https://d11xdyzr0div58.cloudfront.net/static/logos/archlinux-logo-dark-scalable.518881f04ca9.svg)](https://www.archlinux.org)
-
-
-###### TODO
-- [ ] Check `NoExtraxt` option in favor of localepurge
--- archlinux/mkimage-arch.sh
+++ archlinux/mkimage-arch.sh
@@ -14,49 +14,62 @@ hash expect &>/dev/null || {
 	exit 1
 }
 
-
 export LANG="C.UTF-8"
 
 ROOTFS=$(mktemp -d ${TMPDIR:-/var/tmp}/rootfs-archlinux-XXXXXXXXXX)
 chmod 755 $ROOTFS
 
-# packages to ignore for space savings
-PKGIGNORE=(
-    cryptsetup
-    device-mapper
-    dhcpcd
-    iproute2
-    jfsutils
-    linux
-    lvm2
-    man-db
-    man-pages
-    mdadm
-    nano
-    netctl
-    openresolv
-    pciutils
-    pcmciautils
-    reiserfsprogs
-    s-nail
-    systemd-sysvcompat
-    usbutils
-    vi
-    xfsprogs
-)
-IFS=','
-PKGIGNORE="${PKGIGNORE[*]}"
-unset IFS
-
-SNAPSHOT="2016/02/26"
+SNAPSHOT="2016-02-26"
 
 PACMAN_CONF='./pacman.conf'
-PACMAN_MIRRORLIST='Server = https://mirrors.kernel.org/archlinux/$repo/os/$arch'
-PACMAN_MIRRORLIST='Server = https://archive.archlinux.org/repos/'$SNAPSHOT'/$repo/os/$arch'
+PACMAN_MIRRORLIST='Server = https://archive.archlinux.org/repos/'${SNAPSHOT//-//}'/$repo/os/$arch'
 PACMAN_EXTRA_PKGS=''
 EXPECT_TIMEOUT=60
 ARCH_KEYRING=archlinux
-DOCKER_IMAGE_NAME=archlinux
+DOCKER_IMAGE_NAME="archlinux:$SNAPSHOT"
+
+(cat <<-EOF
+	[options]
+	HoldPkg     = pacman glibc
+	Architecture = x86_64
+	SigLevel    = Required DatabaseOptional
+	LocalFileSigLevel = Optional
+
+	NoExtract = usr/lib/systemd/system/*NoExtract=usr/lib/systemd/system/*
+	NoExtract = usr/share/locale/* !usr/share/locale/en_US !usr/share/locale/locale.alias
+
+	IgnorePkg = cryptsetup
+	IgnorePkg = device-mapper
+	IgnorePkg = dhcpcd
+	IgnorePkg = iproute2
+	IgnorePkg = jfsutils
+	IgnorePkg = linux
+	IgnorePkg = lvm2
+	IgnorePkg = man-db
+	IgnorePkg = man-pages
+	IgnorePkg = mdadm
+	IgnorePkg = nano
+	IgnorePkg = netctl
+	IgnorePkg = openresolv
+	IgnorePkg = pciutils
+	IgnorePkg = pcmciautils
+	IgnorePkg = reiserfsprogs
+	IgnorePkg = s-nail
+	IgnorePkg = systemd-sysvcompat
+	IgnorePkg = usbutils
+	IgnorePkg = vi
+	IgnorePkg = xfsprogs
+
+	[core]
+	$PACMAN_MIRRORLIST
+
+	[extra]
+	$PACMAN_MIRRORLIST
+
+	[community]
+	$PACMAN_MIRRORLIST
+EOF
+) > $PACMAN_CONF
 
 export PACMAN_MIRRORLIST
 
@@ -68,7 +81,7 @@ expect <<EOF
 	}
 	set timeout $EXPECT_TIMEOUT
 
-	spawn pacstrap -C $PACMAN_CONF -c -d -G -i $ROOTFS base haveged $PACMAN_EXTRA_PKGS --ignore $PKGIGNORE
+	spawn pacstrap -C $PACMAN_CONF -c -d -G -i $ROOTFS base haveged $PACMAN_EXTRA_PKGS
 	expect {
 		-exact "anyway? \[Y/n\] " { send -- "n\r"; exp_continue }
 		-exact "(default=all): " { send -- "\r"; exp_continue }
@@ -83,12 +96,10 @@ echo 'en_US.UTF-8 UTF-8' > $ROOTFS/etc/locale.gen
 arch-chroot $ROOTFS locale-gen
 arch-chroot $ROOTFS /bin/sh -c 'echo $PACMAN_MIRRORLIST > /etc/pacman.d/mirrorlist'
 
-arch-chroot $ROOTFS /bin/sh -c 'pacman -Scc --noconfirm'
+sed '/^IgnorePkg/d' $PACMAN_CONF > $ROOTFS/etc/pacman.conf
+rm -f $PACMAN_CONF
 
-cp localepurge $ROOTFS/usr/local/bin/localepurge
-chmod +x $ROOTFS/usr/local/bin/localepurge
-cp locale.nopurge $ROOTFS/etc/locale.nopurge
-arch-chroot $ROOTFS /bin/sh -c 'localepurge'
+arch-chroot $ROOTFS /bin/sh -c 'pacman -Scc --noconfirm'
 
 # udev doesn't work in containers, rebuild /dev
 DEV=$ROOTFS/dev
